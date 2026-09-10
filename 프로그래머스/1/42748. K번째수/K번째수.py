780ae1a93f886c65b56a40c9f57c1f0772c09710
Wang's Sweet Home <yog3467@naver.com>
2026-09-10 12:59:12 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,12 +1,6 @@
 def solution(array, commands):
-    answer = []
-    for command in commands:    # [2,5,3]
-        start, end, value = command[0], command[1], command[2]
-        if start == end:
-            del_array = [array[start - 1]]
-        else:
-            del_array = array[start - 1 : end]
-            del_array.sort()
-        answer.append(del_array[value-1])
+    return [sorted(array[s-1:e])[v-1] for s,e,v in commands]
         
-    return answer
+        
+        
+        
