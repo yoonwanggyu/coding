128daee6fb6f923b5a95199ce9b3ecbba8d9146a
Wang's Sweet Home <yog3467@naver.com>
2026-09-10 15:18:31 +0900

    [level 2] Title: 올바른 괄호, Time: 4.17 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,21 @@
+# def solution(s):
+#     count = 0
+#     for i in s:
+#         if i == "(":
+#             count += 1
+#         else:
+#             count -= 1
+#         if count < 0:
+#             return False
+#     return count == 0
+
 def solution(s):
-
-    count = 0
-    for i in s:
-        if i == "(":
-            count += 1
+    stack =[]
+    for ch in s:
+        if ch == "(":
+            stack.append(ch)
         else:
-            count -= 1
-        if count < 0:
-            return False
-    return count == 0
-
-
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
